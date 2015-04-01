e89eea1ea8b3e81889cf2abc744eabdfdaffffef
Debbie Cravey <cinemaestro@yahoo.com>
2015-03-31 22:13:29 -0700

    add initial project files

---
+++ db/migrate/20150401044519_add_attributes_to_messages.rb
@@ -0,0 +1,8 @@
+class AddAttributesToMessages < ActiveRecord::Migration
+  def change
+    add_column  :messages, :to, :string
+    add_column  :messages, :from, :string
+    add_column  :messages, :body, :string
+
+  end
+end
